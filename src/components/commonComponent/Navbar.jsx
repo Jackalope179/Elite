a07b087c0c3f9c48e66f244d20a2c91f4975b557
CanhHoang <nguyencanhhoangnh2019@gmail.com>
2022-04-23 23:22:32 +0700

    add-post

--- src/components/commonComponent/Navbar.jsx
+++ src/components/commonComponent/Navbar.jsx
@@ -212,28 +212,6 @@ export default function Navbar() {
         {renderMenu}
       </Box>
       <Dialog_Form open={open} onClose={handleClose} />
-      {/* <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions>
-      </Dialog> */}
     </>
   );
 }
