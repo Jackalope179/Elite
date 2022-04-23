aa03c7133204bddc3e17861e1567f97b07ca7c87
Hien_Nguyen <73571967+ngochienhv@users.noreply.github.com>
2022-04-23 17:26:36 +0700

    update

--- src/components/postComponent/post.jsx
+++ src/components/postComponent/post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./postStyle.css";
 import PostContent from "../commonComponent/postcontent/postContent";
+
 const post = () => {
   return (
     <div className='container post-container'>
-        <PostContent />
+      <PostContent width={600} hasButtons={true}/>
     </div>
   )
 }
