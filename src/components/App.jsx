1fa58db5b0dc8c1e13f16ce96b275d47e4dddf88
vanh01 <ngvanhfw@gmail.com>
2022-04-23 17:27:35 +0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import Navbar from "./commonComponent/Navbar";
 import SideBar from "./commonComponent/SideBar";
 import RecipeReviewCard from "./postComponent/post";
-import Profile from "./profileComponent/profile";
+import Profile from "./profileComponent/Profile";
 import "../style/App.css";
 const App = () => {
-  return (
-    <>
-      <Navbar id="navbar_" />
-      <div className="container" id="body" style={{ overflow: "hidden" }}>
-        <div className="container">
-          <RecipeReviewCard />
-          <RecipeReviewCard />
-          <RecipeReviewCard />
-          <RecipeReviewCard />
-          <RecipeReviewCard />
-          <RecipeReviewCard />
-        </div>
-        <SideBar />
-      </div>
-      {/* <Profile /> */}
-    </>
-  );
+    return (
+        <>
+            {/* <Navbar id="navbar_" />
+            <div className="container" id="body" style={{ overflow: "hidden" }}>
+                <div className="container">
+                    <RecipeReviewCard />
+                    <RecipeReviewCard />
+                    <RecipeReviewCard />
+                    <RecipeReviewCard />
+                    <RecipeReviewCard />
+                    <RecipeReviewCard />
+                </div>
+                <SideBar />
+            </div> */}
+            <Profile />
+        </>
+    );
 };
 
 export default App;
